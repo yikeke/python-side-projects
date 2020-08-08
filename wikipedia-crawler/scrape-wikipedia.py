b809664e1d51ca8f7fcd3638907fff3f3823e8f7
yikeke <yikeke@pingcap.com>
2020-08-09 01:51:25 +0800

    add wikipedia-crawler

--- wikipedia-crawler/scrape-wikipedia.py
+++ wikipedia-crawler/scrape-wikipedia.py
@@ -2,7 +2,7 @@ import json
 import requests
 from lxml import html
 
-w = open('thesaurus.txt', 'w', encoding='utf-8')
+# code from https://blog.csdn.net/qubeijun/article/details/81509738
 
 def subcat(key):
     s = requests.session()
@@ -12,8 +12,9 @@ def subcat(key):
     wiki = s.get(url)
     tree = html.fromstring(wiki.text)
 
-    print('https://zh.wikipedia.org/wiki/Category:' + key)
-    term.append(key)
+    # print('https://zh.wikipedia.org/wiki/Category:' + key)
+    term.append(url)
+    # term.append(key) # 首行是否带 key
     # 子分类
     subcategories = tree.xpath('//a[@class="CategoryTreeLabel  CategoryTreeLabelNs14 CategoryTreeLabelCategory"]/text()')
     # print(subcategories) 为 0
@@ -25,17 +26,24 @@ def subcat(key):
             pass
         else:
             subcat(s)
-            print(s)
+            # print('subcategories: ' + s + '\n')
 
     for p in pages:
         if p in term:
             pass
         else:
             term.append(p)
-            print(p)
+            # print('pages: ' + p + '\n')
 
 
 term = []
-subcat('前1世纪出生')
+
+w = open('year1902-2020.txt', 'w', encoding='utf-8')
+
+for i in range(1902,2021):
+    search_word = str(i) + '年出生'
+    subcat(search_word)
+
 for t in term:
+    # print(type(str(t)))
     w.write(t+'\n')
