0a98ef8de79a96bc0320fa70bb303f926e8bd10d
yikeke <yikeke@pingcap.com>
2020-09-02 21:59:30 +0800

    Create read-dir.sh

---
+++ shell-scripts/read-dir.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+function read_dir(){
+    for file in `ls $1`       #注意此处这是两个反引号，表示运行系统命令
+    do
+        if [ -d $1"/"$file ]  #注意此处之间一定要加上空格，否则会报错
+        then
+            read_dir $1"/"$file
+        else
+            echo $1"/"$file;   #在此处处理文件即可
+            # python3 scripts/check-tags.py $1"/"$file;
+        fi
+    done
+}   
+#读取第一个参数
+read_dir $1
